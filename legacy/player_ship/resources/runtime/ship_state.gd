c7b6aeb8d3f80a210061139f8b71c80faf8fb47e
Peeqo <fabiankreis@msn.com>
2026-05-09 11:56:36 +0200

    stable cleanup

--- legacy/player_ship/resources/runtime/ship_state.gd
+++
@@ -1,7 +0,0 @@
-class_name ShipState
-extends RefCounted
-
-var is_docked: bool = true
-var docked_body_id: String = ""
-var free_position: Vector2 = Vector2.ZERO
-var last_selected_object_id: String = ""
--- legacy/player_ship/scripts/ship/states/ship_state.gd
+++
@@ -1,7 +0,0 @@
-class_name ShipRuntimeState
-extends RefCounted
-
-var is_docked: bool = true
-var docked_body_id: String = ""
-var free_position: Vector2 = Vector2.ZERO
-var last_selected_object_id: String = ""
